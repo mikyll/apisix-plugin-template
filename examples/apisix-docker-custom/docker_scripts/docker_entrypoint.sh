207db9cdbe607f3808ae367e0f756de651f9c2e2
Michele Righi <righi.michy@gmail.com>
2025-03-10 00:34:02 +0100

    fix: docker custom example
    Still demo.t public-api doesn't work

--- examples/apisix-docker-custom/docker_scripts/docker_entrypoint.sh
+++ examples/apisix-docker-custom/docker_scripts/docker_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm ${APISIX_SOURCE_DIR}/logs/worker_events.sock
+# rm ${APISIX_SOURCE_DIR}/logs/worker_events.sock
 
 # Start ETCD
 mkdir -p ${APISIX_SOURCE_DIR}/logs
@@ -8,12 +8,5 @@ nohup etcd >${APISIX_SOURCE_DIR}/logs/etcd.log 2>&1 &
 
 # Wait for ETCD to be healthy
 until etcdctl endpoint health; do sleep 1; done
-
-# Start APISIX
-apisix start
-
-# Link APISIX logs to
-ln -sf /dev/stdout ${APISIX_SOURCE_DIR}/logs/access.log
-ln -sf /dev/stderr ${APISIX_SOURCE_DIR}/logs/error.log
 
 exec "$@"
